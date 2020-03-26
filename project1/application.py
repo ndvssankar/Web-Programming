59673eb1f0dee6f476b4dce795287da0a49df6e8
ndvssankar <sivashankar@msitprogram.net>
2020-03-26 15:03:49 +0530

    Task 4 Done

--- project1/application.py
+++ project1/application.py
@@ -5,6 +5,9 @@ from flask import Flask, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+# meta = MetaData()
 
 app = Flask(__name__)
 
@@ -17,9 +20,21 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# Creating a database table USERS
+def create_register_table():
+    meta = MetaData()
+    USERS = Table(
+    'USERS', meta, 
+    Column('username', Integer, primary_key = True), 
+    Column('password', String))
+    meta.create_all(engine)
+    print("Database table created successfully")
+
+
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine("postgres://tgsemzadyezlgh:858baa7b0d8b5ce3cb37d6481e187d508a119f33d2f26a2addac896f186eb633@ec2-18-233-137-77.compute-1.amazonaws.com:5432/d389gkjdhv6oa2", echo = True)
 db = scoped_session(sessionmaker(bind=engine))
+create_register_table()
 
 
 @app.route("/")

7c3d4bf6f81c48d26d632f8a802150a1cce0b9cb
ndvssankar <sivashankar@msitprogram.net>
2020-03-30 14:51:48 +0530

    done modularise the code.

--- project1/import.py
+++ project1/import.py
@@ -17,9 +17,11 @@ app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 
-def main():
-    f = open("books.csv")
-    reader = csv.reader(f)
+def load_data_from_csv(file_name):
+    f = open(file_name)
+    return csv.reader(f)
+
+def store_in_db(reader):
     books = []
     c = 0
 
@@ -37,6 +39,9 @@ def main():
             error = str(e.__dict__['orig'])
             app.logger.error("%s", error)
 
+def main():
+    store_in_db(load_data_from_csv("books.csv"))
+    
 if __name__ == "__main__":
     with app.app_context():
         main()
